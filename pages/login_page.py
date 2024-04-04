70d978f4f060e2e0f35cf012816d464df5fea33a
trudoviebudni <mumkoo@gmail.com>
2024-04-04 17:36:49 +0300

    New page is created

---
+++ pages/login_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    LOGIN_PAGE_URL = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+
+    def __init__(self, browser):
+        super().__init__(browser, LoginPage.LOGIN_PAGE_URL)
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Current URL is not correct. It does not contain the handle " \
+                                                    "'login'. "
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form does not exist."
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form does not exist."
