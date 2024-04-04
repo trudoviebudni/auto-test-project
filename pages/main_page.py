51703097bf7ca04913b7094d2cb4c6d2c8704011
trudoviebudni <mumkoo@gmail.com>
2024-04-04 15:53:21 +0300

    Moved the main page link from test-script into MainPage class

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,17 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
+    # так как ссылка постоянная для каждого объекта и по сути является атрибутом класса, создаем константу
+    MAIN_PAGE_URL = "http://selenium1py.pythonanywhere.com/"
+
+    # передаем ссылку в конструктор BasePage
+    def __init__(self, browser):
+        super().__init__(browser, MainPage.MAIN_PAGE_URL)
 
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, '#login_link_invalid')
+
