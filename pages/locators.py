9b501d7242065d2e25e933912d34796bb6f040d5
trudoviebudni <mumkoo@gmail.com>
2024-04-04 22:28:33 +0300

    Added new locator class for ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -18,3 +18,11 @@ class LoginPageLocators:
     REGISTER_FORM_PASSWORD_2 = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_FORM_BUTTON = (By.CSS_SELECTOR, "div.register_form button[name='registration_submit']")
 
+
+class ProductPageLocators:
+    ADD_BUTTON = (By.CSS_SELECTOR, "div.product_main button[type='submit']")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    ALERT_ADDED_TO_BASKET = (By.CSS_SELECTOR, "div.alert:nth-child(1) .alertinner")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, "div.alert:nth-child(1) .alertinner strong")
+    ALERT_BASKET_TOTAL = (By.CSS_SELECTOR, "div.alert:nth-child(3) .alertinner p strong")
