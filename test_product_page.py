34d4733d93d4723cb91375359c7755bd2181fcae
trudoviebudni <mumkoo@gmail.com>
2024-04-14 15:26:48 +0300

    Added new steps to test-case

--- test_product_page.py
+++ test_product_page.py
@@ -21,8 +21,10 @@ def test_guest_can_add_product_to_basket(browser, link):
     # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
     page = ProductPage(browser, link)
     page.open()
+    page.should_not_be_success_message()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_add_message()
     page.check_name_in_alert()
     page.check_basket_total()
+
