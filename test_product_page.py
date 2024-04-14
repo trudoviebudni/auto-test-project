7cffd402fbdd989915e67bb30e2827361e8b086d
trudoviebudni <mumkoo@gmail.com>
2024-04-14 17:15:57 +0300

    Added new test-cases

--- test_product_page.py
+++ test_product_page.py
@@ -16,15 +16,38 @@ links = [
 ]
 
 
-@pytest.mark.parametrize('link', links)
-def test_guest_can_add_product_to_basket(browser, link):
-    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+# @pytest.mark.parametrize('link', links)
+# def test_guest_can_add_product_to_basket(browser, link):
+#     # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+#     page = ProductPage(browser, link)
+#     page.open()
+#     page.add_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.should_be_add_message()
+#     page.check_name_in_alert()
+#     page.check_basket_total()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_add_message()
-    page.check_name_in_alert()
-    page.check_basket_total()
-
+    page.success_message_should_disappear()
