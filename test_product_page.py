a596d51e6b3f1260d369fc3374b9c17add278f22
trudoviebudni <mumkoo@gmail.com>
2024-04-14 17:41:12 +0300

    Added new test cases for login link on product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,7 @@
+import time
+
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
 links = [
@@ -51,3 +54,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.success_message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
