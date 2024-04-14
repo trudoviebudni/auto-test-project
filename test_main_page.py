1f56f4537032dddd5cd1bece97dddea23461cc2a
trudoviebudni <mumkoo@gmail.com>
2024-04-14 18:43:42 +0300

    Added new test-cases for main page and product page

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ import time
 
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -11,3 +12,14 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = MainPage(browser)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_text_message_for_empty_basket()
+
+    
